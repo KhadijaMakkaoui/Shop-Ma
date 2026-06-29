2257e132c6f49034c1e19eeb5ba5b45aadb4a8a3
KhadijaMakkaoui <makkaoui.khadija44@gmail.com>
2026-06-29 12:18:16 +0100

    creat models

--- app/src/main/java/com/example/shopma/models/CartItem.java
+++ app/src/main/java/com/example/shopma/models/CartItem.java
@@ -1,4 +1,25 @@
 package com.example.shopma.models;
 
+
 public class CartItem {
+    private int id;
+    private int productId;
+    private String title;
+    private double price;
+    private int quantity;
+
+    public CartItem(int id, int productId, String title, double price, int quantity) {
+        this.id = id;
+        this.productId = productId;
+        this.title = title;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public int getId() { return id; }
+    public int getProductId() { return productId; }
+    public String getTitle() { return title; }
+    public double getPrice() { return price; }
+    public int getQuantity() { return quantity; }
+    public void setQuantity(int quantity) { this.quantity = quantity; }
 }
--- app/src/main/java/com/example/shopma/models/Commande.java
+++ app/src/main/java/com/example/shopma/models/Commande.java
@@ -1,4 +1,24 @@
 package com.example.shopma.models;
 
+
 public class Commande {
+    private int id;
+    private String date;
+    private int nbArticles;
+    private double montantTotal;
+    private String statut;
+
+    public Commande(int id, String date, int nbArticles, double montantTotal, String statut) {
+        this.id = id;
+        this.date = date;
+        this.nbArticles = nbArticles;
+        this.montantTotal = montantTotal;
+        this.statut = statut;
+    }
+
+    public int getId() { return id; }
+    public String getDate() { return date; }
+    public int getNbArticles() { return nbArticles; }
+    public double getMontantTotal() { return montantTotal; }
+    public String getStatut() { return statut; }
 }
