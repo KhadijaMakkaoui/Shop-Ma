08ca19ecf22c64e3afc89d82c09ee40c2fb79552
KhadijaMakkaoui <makkaoui.khadija44@gmail.com>
2026-07-23 15:07:47 +0100

    ajouter le nombre des produits du panier dans le banner

--- app/src/main/java/com/example/shopma/activities/AccueilActivity.java
+++ app/src/main/java/com/example/shopma/activities/AccueilActivity.java
@@ -8,18 +8,24 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.shopma.R;
+import com.example.shopma.database.DatabaseHelper;
 
 public class AccueilActivity extends AppCompatActivity {
 
     private LinearLayout btnAll, btnElec, btnJewel, btnMen, btnWomen, btnMaps;
-    private TextView tvWelcome;
+    private TextView tvWelcome, tvCartCountBanner;
+    private DatabaseHelper db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accueil);
 
+        db = new DatabaseHelper(this);
+
         tvWelcome = findViewById(R.id.tvWelcome);
+        tvCartCountBanner = findViewById(R.id.tvCartCountBanner);
+
         SharedPreferences pref = getSharedPreferences("UserSession", MODE_PRIVATE);
         String nomUtilisateur = pref.getString("USER_NAME", "Khadija");
         tvWelcome.setText("Bonjour, " + nomUtilisateur);
@@ -64,5 +70,25 @@ public class AccueilActivity extends AppCompatActivity {
             Intent intent = new Intent(AccueilActivity.this, MapsActivity.class);
             startActivity(intent);
         });
+    }
+
+    // Se réexécute automatiquement au retour sur cet écran
+    @Override
+    protected void onResume() {
+        super.onResume();
+        updateCartBannerCount();
+    }
+
+    private void updateCartBannerCount() {
+        if (tvCartCountBanner != null) {
+            int total = db.getQuantiteTotalePanier();
+
+            // Gestion propre du singulier / pluriel
+            if (total <= 1) {
+                tvCartCountBanner.setText(total + " article dans le panier");
+            } else {
+                tvCartCountBanner.setText(total + " articles dans le panier");
+            }
+        }
     }
 }
